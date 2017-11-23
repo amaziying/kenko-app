65a3ca943f97848ea904c949503089c04f1d1040
Ziying Zhang <ziyingzhanginc@gmail.com>
2017-11-23 02:15:50 -0500

    First version of app: recommendations and tips

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -6,9 +6,19 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+	recommendations: Array<{ description: string, link: string }>;
 
   constructor(public navCtrl: NavController) {
-
+  	this.recommendations = [
+  		{
+  			description: "Substitute white bread with whole wheat bread",
+  			link: "xyz.com"
+  		},
+  		{
+  			description: "Reduce consumption of fruit juices and smoothies",
+  			link: "xyz.com"
+  		}
+  	]
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	recommendations: Array<{ title: string, type: string, liked: boolean, link: string }>;
+	like: Function;
+	toggleFavourites: Function;
+	showFavourites: Boolean;
 
+  constructor(public navCtrl: NavController) {
+  	let that = this;
+  	this.recommendations = [
+  		{
+  			title: "What is diabetes?",
+  			type: "Article from Diabetes Canada",
+  			liked: false,
+  			link: "xyz.com"
+  		},
+  		{
+  			title: "Roasting bell peppers",
+  			type: "Recipe",
+  			liked: false,
+  			link: "xyz.com"
+  		}
+  	];
+  	this.showFavourites = false;
+
+  	this.toggleFavourites = function(showFavourites) {
+  		that.showFavourites = showFavourites;
+  	}
+
+  	this.like = function(rec) {
+  		rec.liked = !rec.liked;
+  	}
   }
 
 }
